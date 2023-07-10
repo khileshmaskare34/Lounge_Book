d113b23cb4d8028a3fbc6ea9fdadd2e8b535d6e3
Anmol <anmolrinayat108@gmail.com>
2023-07-10 22:37:48 +0530

    updated by the anmol

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,3 +8,18 @@ var swiper = new Swiper(".mySwiper", {
         disableOnInteraction: false,
        },
   });
+
+
+  
+    /* written by anmol header jisme chhose krna time and station */
+
+    document.getElementById("search-btn").addEventListener("click", function() {
+        const station = document.getElementById("station").value;
+        const checkInTime = document.getElementById("check-in-time").value;
+        const checkOutTime = document.getElementById("check-out-time").value;
+        const beds = document.getElementById("beds").value;
+      
+        // Perform search or validation based on selected values
+        // Add your code here
+      });
+    /* written by anmol header jisme chhose krna time and station ==end */
