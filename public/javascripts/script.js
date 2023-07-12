ab8a5cc054f52987b35d443259d9fd8045af3f3d
Himanshu Bisen <bisenji1799@gmail.com>
2023-07-12 08:42:28 +0530

    hdfbhdbf

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,3 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    duration:2,
-    loop: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-       },
-  });
 
 
   
