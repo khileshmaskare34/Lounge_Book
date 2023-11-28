14104c38002a0d2395a3100fe8058efa3357f9ae
khilesh <khileshmaskare03@gmail.com>
2023-11-28 23:45:41 +0530

    updated

--- module/users.js
+++ module/users.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 // mongoose.set('strictQuery',true);
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/LoungeBookDB") 
+mongoose.connect("mongodb://khileshmaskare03:Khilesh123@ac-kdampfs-shard-00-00.ulpglma.mongodb.net:27017,ac-kdampfs-shard-00-01.ulpglma.mongodb.net:27017,ac-kdampfs-shard-00-02.ulpglma.mongodb.net:27017/?ssl=true&replicaSet=atlas-kpbofv-shard-0&authSource=admin&retryWrites=true&w=majority") 
 .then(function(){
   console.log("connected to db");
 })
